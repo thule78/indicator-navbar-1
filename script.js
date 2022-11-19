f991c70842d51d2541e67fa6dd6b5ce9f3290365
thu le <39904554+thule78@users.noreply.github.com>
2022-11-19 18:35:41 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll(".nav-link");
+const indicator = document.querySelector(".indicator");
+
+navLinks.forEach((navLink, index) => {
+    navLink.addEventListener("click", () =>{
+        document.querySelector(".active").classList.remove("active");
+        navLink.classList.toggle("active");
+        indicator.style.left = `${index * 9.5 + 3}rem`;
+    })
+       
+})
